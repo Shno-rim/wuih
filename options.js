f4aa73b68232aac969d7f9462997933a8ac8472b
Shino <90832242+Shno-rim@users.noreply.github.com>
2021-12-07 17:21:05 +0700

    Update options.js

--- options.js
+++ options.js
@@ -1,48 +1,18 @@
-const fs = require('fs-extra')
-
-module.exports = options = (nerr, start) => {
-    // const chromePath = {
-    //     win32: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe', // Windows 32 bit
-    //     win64: 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe', // Windows 64 bit
-    //     linuxChrome: '/usr/bin/google-chrome-stable', // Linux - Chrome
-    //     linuxChromium: '/usr/bin/chromium-browser', // Linux - Chromium
-    //     darwin: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome' // MacOS
-    // }
-
-    // if (fs.existsSync(chromePath.win32)) {
-    //     execPath = chromePath.win32
-    // } else if (fs.existsSync(chromePath.win64)) {
-    //     execPath = chromePath.win64
-    // } else if (fs.existsSync(chromePath.linuxChrome)) {
-    //     execPath = chromePath.linuxChrome
-    // } else if (fs.existsSync(chromePath.linuxChromium)) {
-    //     execPath = chromePath.linuxChromium
-    // } else if (process.platform === 'darwin') {
-    //     execPath = chromePath.darwin
-    // } else {
-    //     console.error(new Error('Google Chrome Is Not Installed'))
-    //     process.exit(1)
-    // }
-
-    const options = {
-        nerr: nerr,
-        autoRefresh: true,
-        restartOnCrash: start,
-        cacheEnabled: false,
-        // executablePath: execPath,
-        useChrome: true,
-        killProcessOnBrowserClose: true,
-        throwErrorOnTosBlock: false,
-        chromiumArgs: [
-            '--no-sandbox',
-            '--disable-setuid-sandbox',
-            '--aggressive-cache-discard',
-            '--disable-cache',
-            '--disable-application-cache',
-            '--disable-offline-load-stale-cache',
-            '--disk-cache-size=0'
-        ]
-    }
-
-    return options
+{
+     "ownerNumber": ["62859194143712@s.whatsapp.net","6281296907289@s.whatsapp.net","6282371255459@s.whatsapp.net","6289697228713@s.whatsapp.net","6285279655886@s.whatsapp.net"],
+     "ownerName": "Wira",
+     "ownerNao" : "Dwi Rizqi",
+     "ownerSagiri" : "Hilmy",
+     "gamewaktu": 60,
+     "owner": "62859194143712",
+     "ownerNNao" : "6281358181668",
+     "ownerNSagiri" : "6281217779427",
+     "botName": "Ｎｅｒｏ",
+     "lolkey": "ciko",
+     "vhtearkey": "genbotkey",
+     "zekskey": "NinoWangy",
+     "leysc": "dappakntlll",
+     "xteamkey": "e7ff45d7f2085936",
+     "txtDonasi": "Dana : 0859194143712\nTrakteer : https://trakteer.id/ShikoWangy",
+     "gcount": 100
 }
