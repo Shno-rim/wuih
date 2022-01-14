18a2a8597e75e7a26511e7acecea4cb96bcb0bb3
Shino <90832242+Shno-rim@users.noreply.github.com>
2022-01-14 17:06:25 +0700

    Add files via upload

---
+++ message/welcome.js
@@ -0,0 +1,73 @@
+const {
+	MessageType
+} = require("@adiwajshing/baileys");
+const fs = require("fs-extra")
+
+const { getBuffer } = require('../lib/myfunc')
+const { color, bgcolor } = require('../lib/color')
+join = 'Selamat Datang'
+leave = 'Selamat Tinggal'
+
+teks = `Selamat Datang`
+let setting = JSON.parse(fs.readFileSync('./options.json'))
+
+module.exports = welcome = async (shino, anu) => {
+	    const welkom = JSON.parse(fs.readFileSync('./database/group/welcome.json'))
+	    const isWelcome = welkom.includes(anu.jid)
+	    if (!isWelcome) return
+		try {
+			    mem = anu.participants[0]
+			    console.log(anu)
+                try {
+                pp_user = await shino.getProfilePicture(mem)
+                } catch (e) {
+                pp_user = 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png?q=60'
+            }
+                try {
+                pp_grup = await shino.getProfilePicture(anu.jid)
+                } catch (e) {
+                pp_grup = 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png?q=60'
+            }
+            if (anu.action == 'add' && mem.includes(shino.user.jid)) {
+            shino.sendMessage(anu.jid, 'Halo! Terima Kasih sudah Mengundangku, Jika ingin Menggunakan Bot Ketik !menu', 'conversation')
+            }
+             if (anu.action == 'add' && !mem.includes(shino.user.jid)) {
+             if (!welkom.includes(anu.jid)) return
+                mdata = await shino.groupMetadata(anu.jid)
+           
+                memeg = mdata.participants.length
+            	num = anu.participants[0]
+                let v = shino.contacts[num] || { notify: num.replace(/@.+/, '') }
+                anu_user = v.vname || v.notify || num.split('@')[0]
+            
+       buff = await getBuffer(`https://api.lolhuman.xyz/api/base/welcome?apikey=${setting.lolkey}&img1=${pp_user}&img2=${pp_grup}&background=https://telegra.ph/file/559d40a73f54e257b0b2e.jpg&username=${encodeURI(anu_user)}&member=${memeg}&groupname= ${encodeURI(mdata.subject)}`)
+    
+ await shino.sendMessage(buff, "imageMessage", {
+
+            thumbnail: buff})     }
+
+      if (anu.action == "remove" && !mem.includes(shino.user.jid)) {
+
+        mdata = await shino.groupMetadata(anu.jid);
+
+        num = anu.participants[0];
+
+        let w = shino.contacts[num] || { notify: num.replace(/@.+/, "") };
+
+        anu_user = w.vname || w.notify || num.split("@")[0];
+
+        memeg = mdata.participants.length;
+
+        out = `${leave}`;
+
+        ry = await getBuffer(`https://api.lolhuman.xyz/api/base/leave?apikey=${setting.lolkey}&img1=${pp_user}&img2=${pp_grup}&background=https://telegra.ph/file/559d40a73f54e257b0b2e.jpg&username=${encodeURI(anu_user)}&member=${memeg}&groupname= ${encodeURI(mdata.subject)}`)
+            
+await shino.sendMessage(from,ry, "imageMessage", {
+
+            thumbnail: ry})
+
+        }
+		} catch (e) {
+			console.log('Error : %s', color(e, 'red'))
+		}
+	}
